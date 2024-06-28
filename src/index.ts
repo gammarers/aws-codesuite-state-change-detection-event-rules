d5ad9e8c261a6913389e5ed06c0b025b5a275954
yicr <yicr@users.noreply.github.com>
2024-06-28 18:09:40 +0900

    feat!: first happy code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import * as events from 'aws-cdk-lib/aws-events';
+import { Construct } from 'constructs';
+
+type omitKeys = 'eventPattern';
+
+export interface CodePipelineStateChangeDetectionEventRuleProps extends Omit<events.RuleProps, omitKeys> {}
+
+export class CodePipelineStateChangeDetectionEventRule extends events.Rule {
+  constructor(scope: Construct, id: string, props: CodePipelineStateChangeDetectionEventRuleProps) {
+    super(scope, id, {
+      ...props,
+      eventPattern: {
+        source: ['aws.codepipeline'],
+        detailType: ['CodePipeline Pipeline Execution State Change'],
+      },
+    });
   }
 }
---
+++ test/rule.test.ts
@@ -0,0 +1,30 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as events from 'aws-cdk-lib/aws-events';
+import { CodePipelineStateChangeDetectionEventRule } from '../src';
+
+describe('Rule Check', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'TestingStack', {
+    env: {
+      account: '123456789012',
+      region: 'us-east-1',
+    },
+  });
+
+  const rule = new CodePipelineStateChangeDetectionEventRule(stack, 'CodePipelineStateChangeDetectionEventRule', {
+    ruleName: 'codepipeline-state-change-detection-event-rule',
+  });
+
+  it('Is Rule', () => {
+    expect(rule).toBeInstanceOf(events.Rule);
+  });
+
+  const template = Template.fromStack(stack);
+
+  it('Should match snapshot.', () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+
+});
