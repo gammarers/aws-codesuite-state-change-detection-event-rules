781ac00f7e672ff14a0559f3d52e259070af4f09
yicr <yicr@users.noreply.github.com>
2024-07-02 13:01:52 +0900

    chore: workflow node version set 22.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -20,7 +20,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     },
   },
   minNodeVersion: '18.0.0',
-  workflowNodeVersion: '22.2.0',
+  workflowNodeVersion: '22.x',
   autoApproveOptions: {
     secret: 'GITHUB_TOKEN',
     allowedUsernames: ['yicr'],
